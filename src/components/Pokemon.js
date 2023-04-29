aacfc0d7c675b2a0f5177ba6f31772d3e82512b2
mattiacoll <64754556+mattiacoll@users.noreply.github.com>
2023-04-29 19:46:20 +0200

    Added unit test for pokemon component

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -6,21 +6,24 @@ import { colors } from './colors';
 
 const Pokemon = ({ info }) => {
 
+  if ( !info )
+    return;
+
   const {
-    name,
-    image,
-    number,
-    color,
+    color  = '',
+    image  = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/0.png',
+    name   = '',
+    number = 0,
   } = info;
 
   return (
     <Link 
-      className={ pokeEl } 
-      to={`/pokemon/${ number }`} 
+      className={ pokeEl }
+      to={`/pokemon/${ number }`}
       style={{
         backgroundColor: colors[color],
       }}
-      data-testid="pokemon"
+      data-testid="pnkm-wrap"
     >
       <div className={ pokeImgWrap }>
         <img
@@ -30,10 +33,11 @@ const Pokemon = ({ info }) => {
           width="96"
           height="96"
           loading="lazy"
+          data-testid="pkmn-img"
         />
       </div>
-      <h2 className={ pokeH2 }>{ name }</h2>
-      <p className={ pokeP }>{ number.toString().padStart( 3, '0' ) }</p>
+      <h2 data-testid="pkmn-tit" className={ pokeH2 }>{ name }</h2>
+      <p data-testid="pkmn-num" className={ pokeP }>{ number.toString().padStart( 3, '0' ) }</p>
     </Link>
   );
 
---
+++ src/components/pokemon.test.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import Pokemon from './Pokemon';
+
+describe( 'Render Pokemon', () => {
+
+  test( 'With complete props', () => {
+
+    const { queryByTestId } = render( <Pokemon info={{
+      color: 'green',
+      image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png',
+      name:  'Bulbasaur',
+      number: 1,
+    }} /> );
+
+    expect( queryByTestId( 'pkmn-img' ) ).not.toBeNull();
+
+    expect( queryByTestId( 'pkmn-tit' ) ).not.toBeNull();
+    expect( queryByTestId( 'pkmn-tit' ) ).toHaveTextContent( 'Bulbasaur' );
+
+    expect( queryByTestId( 'pkmn-num' ) ).not.toBeNull();
+    expect( queryByTestId( 'pkmn-num' ) ).toHaveTextContent( '1' );
+
+  });
+
+  test( 'With no props', () => {
+
+    const { queryByTestId } = render( <Pokemon /> );
+
+    // Assertion
+    expect( queryByTestId( 'pnkm-wrap' ) ).toBeNull();
+
+  });
+
+  test( 'With partial props', () => {
+
+    const { queryByTestId } = render( <Pokemon info={{
+      color: 'green',
+    }} /> );
+
+    expect( queryByTestId( 'pkmn-img' ) ).not.toBeNull();
+    expect( queryByTestId( 'pkmn-tit' ) ).not.toBeNull();
+    expect( queryByTestId( 'pkmn-num' ) ).not.toBeNull();
+
+  });
+
+  test( 'With wrong props types', () => {
+
+    const { queryByTestId } = render( <Pokemon info={{
+      name:   null,
+      number: 'fw',
+      image:  undefined,
+    }} /> );
+
+    expect( queryByTestId( 'pkmn-img' ) ).not.toBeNull();
+    expect( queryByTestId( 'pkmn-tit' ) ).not.toBeNull();
+    expect( queryByTestId( 'pkmn-num' ) ).not.toBeNull();
+
+  });
+
+});
