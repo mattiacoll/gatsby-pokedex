3b7ec9cc9b845b72806a7a3675332346b361760f
mattiacoll <64754556+mattiacoll@users.noreply.github.com>
2023-04-29 17:34:30 +0200

    Diveded Pokedex spec from Pokemon

--- tests/pokedex.spec.js
+++ tests/pokedex.spec.js
@@ -5,6 +5,7 @@ test.describe( 'List page', () => {
 
   test.beforeEach( async ({ page }) => {
     await page.goto( 'http://localhost:8000/' );
+    await page.waitForLoadState();
   });
 
 
@@ -66,10 +67,8 @@ test.describe( 'List page', () => {
       expect( input ).not.toBeNull();
       await input.type( 'bulbasaur' );
 
-      const pokemons = page.getByTestId( 'pokemon' ),
-        numPokemons  = await pokemons.count();
-
-      expect( numPokemons ).toEqual( 1 );
+      const pokemons = page.getByTestId( 'pokemon' );
+      await expect( pokemons ).toHaveCount( 1 );
 
     });
 
@@ -78,74 +77,6 @@ test.describe( 'List page', () => {
       const pokemons = page.getByTestId( 'pokemon' );
 
       await pokemons.first().click();
-
-      page.on( 'response', ( response ) => {
-        expect( response.status() ).not.toBe( 404 );
-      });
-
-    });
-
-  });
-
-});
-
-test.describe( 'Pokemon page', () => {
-
-  test.beforeEach( async ({ page }) => {
-    await page.goto( 'http://localhost:8000/pokemon/1/' );
-  });
-
-
-  test.describe( 'Structure', () => {
-    test( 'Has navigation in 3 or more languages', async ({ page }) => {
-
-      const navEls = page.locator( 'nav > a' ),
-        navNums    = await navEls.count();
-
-      expect( navNums ).toBeGreaterThanOrEqual( 3 );
-
-      for ( const el of await navEls.all() ) {
-
-        const attr = await el.getAttribute( 'href' );
-        expect( attr ).not.toBeNull();
-
-      }
-
-    });
-
-    test( 'Has pokemon information', async ({ page }) => {
-
-      // text information
-      [
-        'pkmn-name',
-        'pkmn-num',
-        'pkmn-desc',
-        'pkmn-genus',
-      ].forEach( ( testID ) => {
-
-        const element = page.getByTestId( testID );
-
-        expect( element ).not.toBeNull();
-        expect( element ).not.toBeEmpty();
-
-      });
-
-      // image
-      const element = page.getByTestId( 'pkmn-img' );
-      expect( element ).not.toBeNull();
-      expect( await element.getAttribute( 'src' ) ).not.toBeNull();
-
-    });
-  });
-
-
-  test.describe( 'Functionality', () => {
-
-    test( 'Navigate to a different language', async ({ page }) => {
-
-      const navEls = page.locator( 'nav > a' );
-
-      await navEls.nth( 2 ).click();
 
       page.on( 'response', ( response ) => {
         expect( response.status() ).not.toBe( 404 );
